ac4289f463a7ecfae1426d805840590d1ba0e05a
Tyler Jensen <troubletimestwo97@gmail.com>
2022-11-18 09:40:28 -0700

    pulled to push

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const express = require('express')
-const cors = require('cors')
 const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.static('Pics'))
-
-app.use(cors())
 
 //endpoints
 app.get("/", (req, res) => {
